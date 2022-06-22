84556e1589dfedb0412f6b2ca1e35ea6bae0c166
SlothSan <mikeoram01@gmail.com>
2022-06-22 10:40:23 +0100

    Basic math functions created and tested.

---
+++ script.js
@@ -0,0 +1,20 @@
+function add (a, b) {
+    console.log( a + b );
+}
+
+function subtract (a, b) {
+    console.log( a - b );
+}
+
+function multiply (a, b) {
+    console.log( a * b );
+}
+
+function divide (a, b) {
+    console.log( a / b );
+}
+
+add(12, 8);
+subtract(10, 11);
+multiply(10, 50);
+divide(1251, 4);
