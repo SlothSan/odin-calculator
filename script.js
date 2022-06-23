4fcc4b8d5da0a332a2d435fa15fabd4f044403cb
SlothSan <mikeoram01@gmail.com>
2022-06-23 10:53:50 +0100

    ClearDisplay + Button testing. Need to work on operating next - fun.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-
+//Global Variables 
+const display = document.getElementById("display");
+let value;
+let newValue = 7;// remove after testing.
+console.log(display);
+console.log(value);
 //Maths Functions
-function add (a, b) {
-    console.log( a + b );
+function add (value, newValue) {
+    console.log( value + newValue );
 }
 
 function subtract (a, b) {
@@ -17,10 +22,10 @@ function divide (a, b) {
 }
 
 //Operate Function
-function operate (a, operator, b) {
+function operate (value, operator, newValue) {
     switch (operator) {
         case "+":
-            add(a, b);
+            add(value, newValue);
             break;
         case "-":
             subtract(a, b);
@@ -34,6 +39,27 @@ function operate (a, operator, b) {
         default: 
             console.log("You shouldn't see this");
     }
+}
+
+//Change calcs display and return the value for operating. 
+function changeDisplay(val){
+    if(value == undefined) {
+        value = parseInt(val, 10);
+        display.innerText = value;
+        return value;
+    } else {
+        value = "" + value + parseInt(val, 10);
+        display.innerText = value;
+        value = parseInt(value, 10);
+        return value;
+    }
+}
+
+//Clear Calc and set value back to undefined.
+function clearCalc() {
+    value = undefined
+    display.innerText = "";
+    return value;
 }
 
 operate(10, "/", 10);
