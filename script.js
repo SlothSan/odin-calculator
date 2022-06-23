7581a4252842389e89a72d4a23ca423d7df684d6
SlothSan <mikeoram01@gmail.com>
2022-06-23 15:07:48 +0100

    inputOperand implemented and tested. Clear added and tested.

--- script.js
+++ script.js
@@ -27,6 +27,9 @@ function addEventButtons() {
                 console.log("I am an operand!")
                 inputOperand(buttons[i].value);
                 updateDisplay();
+            } else if (buttons[i].classList.contains(`clear`)) {
+                clearDisplay();
+                updateDisplay();
             }
         });
     }
@@ -36,6 +39,39 @@ addEventButtons();
 
 //Function for inputing operands. 
 function inputOperand(operand){
+    if (firstOperator === null) {
+        if(displayValue === `0` || displayValue === 0) {
+            displayValue = operand;
+        } else if (displayValue === firstOperand){
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
+    } else {
+        if(displayValue === firstOperand) {
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
+    }
+};
 
 
+
+
+
+
+
+
+
+
+
+//Clear display function / reset all variables 
+function clearDisplay() {
+    displayValue = 0;
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
 }
